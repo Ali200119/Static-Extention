01cc0621527dd074829b33c75c49d47244e7fb54
Ali200119 <aliit@code.edu.az>
2022-12-22 00:52:28 +0400

    feat: Task 1

---
+++ Static_Extentions/AtHome/Helpers/Extentions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace AtHome.Helpers
+{
+	public static class Extentions
+	{
+		public static int Factorial(this int number)
+		{
+			int factorial = 1;
+
+			for (int i = 1; i <= number; i++)
+			{
+				factorial *= i;
+			}
+
+			return factorial;
+		}
+	}
+}
+
---
+++ Static_Extentions/AtHome/Program.cs
@@ -0,0 +1,20 @@
+﻿using AtHome.Helpers;
+
+#region Task 1
+
+Console.WriteLine("Please enter the number whose factorial you want to find:");
+Number: string checkNumber = Console.ReadLine();
+int num;
+bool isNumber = int.TryParse(checkNumber, out num);
+
+if (!isNumber)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Please enter the number:");
+    Console.ResetColor();
+    goto Number;
+}
+
+Console.WriteLine(num.Factorial());
+
+#endregion
